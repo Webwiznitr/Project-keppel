63b5fd492f3cc516963c87b22addf5ccbb851975
Sneha Mallik <snehamallik665@gmail.com>
2020-12-06 21:12:04 +0530

    Minor styling added

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -17,7 +17,7 @@ function App() {
   return (
     <div>
       <h1>
-        <span>clubopedia</span>
+        <span>CLUBOPEDIA</span>
       </h1>
       <dl className = "dictionary">
       {content.map(createCard)}
