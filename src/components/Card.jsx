5bdf771531ce54ab5ce9cb4f45f2cd0dd5620044
Aniket762 <aniketindian8@gmail.com>
2020-12-09 02:01:54 +0530

    Social Media

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,7 +3,10 @@ import FacebookIcon from "@material-ui/icons/Facebook"
 import TwitterIcon from "@material-ui/icons/Twitter"
 import InstagramIcon from "@material-ui/icons/Instagram"
 import LinkedInIcon from "@material-ui/icons/LinkedIn"
-
+import YouTubeIcon from '@material-ui/icons/YouTube';
+import TelegramIcon from '@material-ui/icons/Telegram';
+import LanguageIcon from '@material-ui/icons/Language';
+import BookIcon from '@material-ui/icons/Book';
 
 function Card(props) {
   return (
@@ -14,10 +17,33 @@ function Card(props) {
           <img src={props.img} className="img-box"></img>
           <span>{props.name}</span>
           <div className="social">
+          <a href="https://aniket.live" className="social-icons facebook">
           <FacebookIcon></FacebookIcon>
+          </a>
+          <a href="https://aniket.live" className="social-icons twitter" >
           <TwitterIcon></TwitterIcon>
+          </a>
+          <a href="https://aniket.live" className="social-icons instagram" >
           <InstagramIcon></InstagramIcon>
+          </a>
+          <a href="https://aniket.live" className="social-icons linkedin" >
           <LinkedInIcon></LinkedInIcon>
+          </a>
+          <a href="https://aniket.live" className="social-icons youtube" >
+          <YouTubeIcon></YouTubeIcon>
+          </a>
+          <a href="https://aniket.live" className="social-icons telegram" >
+          <TelegramIcon></TelegramIcon>
+          </a>
+          <a href="https://aniket.live" className="social-icons blog" >
+          <BookIcon></BookIcon>
+          </a>
+          <a href="https://aniket.live" className="social-icons portfolio" >
+          <LanguageIcon></LanguageIcon>
+          </a>
+          
+          
+          
           </div>
           
         </dt>
